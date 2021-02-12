706df63b4d74b30d0a2bb52deeffe81287517c9a
Samus <26967164+SamusElderg@users.noreply.github.com>
2021-02-12 10:24:48 +0800

    fix apex charts issue (need to use array)
    apex requires an array for series otherwise it throws a critical error
    
    simply wrap the series in square brackets to fix it

--- src/components/Sections/PositionComponent.js
+++ src/components/Sections/PositionComponent.js
@@ -313,7 +313,7 @@ const PositionComponent = (props) => {
                         <div className="mt-4 mt-sm-0">
                             <ReactApexChart
                                 options={options}
-                                series={formatAllUnits(props.userPC * 100)}
+                                series={[formatAllUnits(props.userPC * 100)]}
                                 type="radialBar"
                                 height="180"
                             />
